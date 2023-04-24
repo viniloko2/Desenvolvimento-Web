a77b2b18f8ac25b95824f865649400c15a3cecc9
viniloko2 <114232800+viniloko2@users.noreply.github.com>
2023-04-24 14:15:32 -0300

    Delete sorteador directory

--- sorteador/sorte.js
+++
@@ -1,6 +0,0 @@
-function sortear() {
-    var minimo = parseInt(document.getElementById("minimo").value);
-    var maximo = parseInt(document.getElementById("maximo").value);
-    var numero = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
-    document.getElementById("resultado").innerHTML = "O número sorteado foi: " + numero;
-  }
